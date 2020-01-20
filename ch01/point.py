bb499ea87ed2b1bf8ebb046776c654a9bf9bf6e2
sajacaros <sajacall82@naver.com>
2020-01-20 17:07:53 +0900

    ch01

---
+++ ch01/point.py
@@ -0,0 +1,11 @@
+class Point(object):
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
+
+def locate(latitue: float, longuitude: float) -> Point:
+    """맵에서 좌에 해당하는 객체를 검색"""
+    pass
+
+print(locate.__doc__)
+
